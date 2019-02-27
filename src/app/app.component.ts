63c7c10143e051a3a7eec2e7ded60d51f1da415e
SHAUN DALY - STUDENT <G00286445@gmit.ie>
2019-02-27 17:37:40 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService} from './services/data.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'data-app';
+export class AppComponent implements OnInit{
+  students:any = [];
+  weather:any = [];
+  temp:any = [];
+
+  constructor(private dataService:DataService){}
+
+  ngOnInit(){
+    this.dataService.GetStudentData().subscribe((data)=>{
+      this.students = data.students;
+    }); // Asyncrhonus method
+
+    this.dataService.GetWeatherData().subscribe((data)=>{
+      this.weather = data.weather;
+      this.temp = data.main;
+    });
+
+  }
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://www.jsonblob.com/api/jsonblob/16b235cd-3aaa-11e9-9959-f3b7e51e4674');
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&APPID=4f595d63c4c28d20e751f2d2a9bbf814');
+  }
+}
